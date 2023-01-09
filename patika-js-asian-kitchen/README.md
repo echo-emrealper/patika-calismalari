49c694ef54093db78dfd6d9dc43261c2a07addc3
hipotetik <emrealper@mail.com>
2023-01-09 18:19:54 +0300

    Upload files and folders.

---
+++ patika-js-asian-kitchen/README.md
@@ -0,0 +1,16 @@
+# Javascript ile Lorem Picsum Navigator
+
+Bu repo [Kodluyoruz](https://www.kodluyoruz.org) [Patika-JS](https://app.patika.dev/courses/javascript) eğitimleri kapsamındadır.
+
+## Çalışma İndirme Linki
+
+[Çalışma Klasörü Linki (Dropbox)](https://www.dropbox.com/sh/22u6ww6a9lt4rlr/AAAnjjBgEPF1Dp9SVecTX0tJa?dl=0)
+
+* Eğitimlerde oluşturulan projeler ve ödevler kategorize edilerek tek bir repo (eğitim reposu) içerisinde klasörlenmiştir.
+
+* İlgili çalışmalara ait klasörlerin eğitmen/denetmenlerce rahatça indirilerek incelenebilmesi için kendine ait depolama/bulut linkleri yukarıdaki gibi verilmiştir.
+
+## Preview
+
+![echo-emrealper](media/ss-01.jpg)
+![echo-emrealper](media/ss-02.jpg)
---
+++ patika-js-asian-kitchen/js/app.js
@@ -0,0 +1,65 @@
+// import from another js files
+import { menu } from "./menu.js";
+
+//variables
+const btnSection = document.querySelector('.btn-container');
+const menuSection = document.querySelector('.section-center');
+const foodList = [...new Set(menu.map(item => item.category))];
+
+//main function call
+starter(window, foodList, btnSection);
+
+//main function
+function starter(loader, items, listener) {
+    //page start event
+    loader.addEventListener('load', createMenu(menu));
+    
+    //items list to create buttons
+    items.sort((a, b) => a.localeCompare(b));
+    items.unshift('All');
+    items.map(item => listener.appendChild(createButton(item)));
+
+    //button listeners
+    listener.childNodes.forEach(item => item.addEventListener('click', filterMenu));
+}
+
+//to create buttons for foodList
+function createButton(category) {
+    const btn = document.createElement('button');
+    btn.classList.add('btn', 'btn-item', 'btn-outline-dark');
+    btn.id = category;
+    btn.textContent = category;
+    return btn;
+}
+
+//to create menu items --> call by CreateMenu for foodList
+function createMenuItem(item) {
+    let menuItem = `<div class = "menu-items col-lg-6 col-sm-12">
+                    <img src=${item.img} alt= ${item.title} class = "photo">
+                    <div class = "menu-info">
+                        <div class = "menu-title">
+                            <h4>${item.title}</h4>
+                            <h4 class="price">${item.price}</h4>
+                        </div>
+                        <div class="menu-text">${item.desc}</div>
+                    </div>
+                </div>`
+    return menuItem;
+}
+
+//to create menu
+function createMenu(items) {
+    items.forEach(item => menuSection.innerHTML += createMenuItem(item))
+};
+
+//to filter menu
+function filterMenu(el) {
+    menuSection.innerHTML = '';
+    menu.filter(item => {
+        if (el.target.id === 'All') {
+            menuSection.innerHTML += createMenuItem(item)
+        } else if ((el.target.id === item.category)) {
+            menuSection.innerHTML += createMenuItem(item);
+        }
+    })
+}
