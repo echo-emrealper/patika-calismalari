f24a91927d0b7e3ec58b1c109c03ed63c0d4c7ce
echo-emrealper <emrealper@mail.com>
2023-02-20 14:40:48 +0300

    Static files uploaded

--- patika-nodejs-pcat-projesi/app.js
+++ patika-nodejs-pcat-projesi/app.js
@@ -1,16 +1,34 @@
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 const port = 3000;
 
+/* const myLogger = (req, res, next) => {
+    console.log('middleware 1');
+    next();
+};
+
+const myLogger2 = (req, res, next) => {
+    console.log('middleware 2');
+    next();
+}; */
+
+//MIDDLEWARES
+app.use(express.static('public'));
+/* app.use(myLogger);
+app.use(myLogger2); */
+
 app.get('/', (req, res) => {
-    const photo = {
+    /*     const photo = {
         id: 1,
-        name: "Photo Name",
-        description: "Photo description"
+        name: 'Photo Name',
+        description: 'Photo description',
     };
-    res.status(200).send(photo);
+    res.status(200).send(photo); */
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 app.listen(port, () => {
