6a9d0a3de53b7732e61af1e740a4347d44312086
echo-emrealper <emrealper@mail.com>
2023-02-21 11:06:44 +0300

    Routes changed.

--- patika-nodejs-clean-blog-project/app.js
+++ patika-nodejs-clean-blog-project/app.js
@@ -6,28 +6,50 @@ const app = express();
 
 const port = 3000;
 
-//MIDDLEWARES
+// MIDDLEWARES
 app.use(express.static('public'));
 
-//TEMPLATE ENGINE
+// TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
-//ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+// ROUTES
+app.get('*', (req, res) => {
+  switch (req.url) {
+    case '/':
+      res.render('index');
+      break;
+    case '/about':
+      res.render('about');
+      break;
+    case '/add_post':
+      res.render('add_post');
+      break;
+    case '/post':
+      res.render('post');
+      break;
+    default:
+      res.send('404: Page Not Found', 404);
+      break;
+  }
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
-});
+/* // ROUTES: STANDART
+  app.get('/', (req, res) => {
+    res.render('index');
+  });
 
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
+  app.get('/about', (req, res) => {
+    res.render('about');
+  });
 
-app.get('/post', (req, res) => {
-  res.render('post');
-});
+  app.get('/add_post', (req, res) => {
+    res.render('add_post');
+  });
+
+  app.get('/post', (req, res) => {
+    res.render('post');
+  }); 
+ */
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
