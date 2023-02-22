a44c4d4bc845290713ac9ccfef06e4d47296b75c
echo-emrealper <emrealper@mail.com>
2023-02-22 17:25:25 +0300

    Update lessons.

--- patika-nodejs-pcat-projesi/app.js
+++ patika-nodejs-pcat-projesi/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const ejs = require('ejs');
-
 const path = require('path');
-
 const app = express();
-
 const port = 3000;
+const Photo = require('./models/Photo.js');
 
 /* const myLogger = (req, res, next) => {
     console.log('middleware 1');
@@ -18,16 +16,22 @@ const myLogger2 = (req, res, next) => {
     next();
 }; */
 
-//TEMPLATE ENGINE
+// MONGOOSE CONNECT
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
+
+// TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
-//MIDDLEWARES
+// MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 /* app.use(myLogger);
 app.use(myLogger2); */
 
-//ROUTES
-app.get('/', (req, res) => {
+// ROUTES
+app.get('/', async (req, res) => {
     /* const photo = {
         id: 1,
         name: 'Photo Name',
@@ -35,7 +39,11 @@ app.get('/', (req, res) => {
     };
     res.status(200).send(photo); */
     // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-    res.render('index');
+
+    const photos = await Photo.find({});
+    res.render('index', {
+        photos
+    });
 });
 
 app.get('/about', (req, res) => {
@@ -48,6 +56,11 @@ app.get('/add', (req, res) => {
 
 app.get('/video-page', (req, res) => {
     res.render('video-page');
+});
+
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body);
+    res.redirect('/');
 });
 
 app.listen(port, () => {
