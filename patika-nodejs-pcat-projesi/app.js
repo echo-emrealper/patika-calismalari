57edbdeeb51b986b37199b1c65bfda71f00ee5f6
echo-emrealper <emrealper@mail.com>
2023-02-26 21:50:33 +0300

    Uploads lessons.

--- patika-nodejs-pcat-projesi/app.js
+++ patika-nodejs-pcat-projesi/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 const Photo = require('./models/Photo.js');
@@ -27,6 +29,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 /* app.use(myLogger);
 app.use(myLogger2); */
 
@@ -40,7 +43,7 @@ app.get('/', async (req, res) => {
     res.status(200).send(photo); */
     // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 
-    const photos = await Photo.find({});
+    const photos = await Photo.find({}).sort('-dateCreated');
     res.render('index', {
         photos,
     });
@@ -68,8 +71,24 @@ app.get('/photos/:id', async (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-    await Photo.create(req.body);
-    res.redirect('/');
+    // console.log(req.files.image);
+    // await Photo.create(req.body);
+    // res.redirect('/');
+    const uploadDir = 'public/uploads';
+
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir);
+    }
+
+    let uploadedImage = req.files.image;
+    let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+    uploadedImage.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + uploadedImage.name,
+        });
+        res.redirect('/');
+    });
 });
 
 app.listen(port, () => {
